1e072d647a6f18917e7ce78df62e4619a3d3f656
Emily Read <emilyread01@gmail.com>
2021-08-10 12:26:18 -0700

    added method to read one item

--- src/main/java/com/example/mvc/controllers/BooksController.java
+++ src/main/java/com/example/mvc/controllers/BooksController.java
@@ -8,8 +8,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.mvc.models.Book;
 import com.example.mvc.services.BookService;
@@ -22,7 +24,8 @@ public class BooksController {
 	 public BooksController(BookService bookService) {
 	        this.bookService = bookService;
 	    }
-	
+	 
+	 //SHOW ALL BOOKS
 	@RequestMapping("/books")
     public String index(Model model) {
         List<Book> books = bookService.allBooks();
@@ -30,6 +33,22 @@ public class BooksController {
         return "/books/index.jsp";
     }
 	
+	//SHOW ONE BOOK
+//	@RequestMapping("/books/{id}")
+//	public String show(Model model, @RequestParam(value="id") Long id) {
+//	    Book book = bookService.findBook(id);
+//	    model.addAttribute("book", book);
+//	    return "/books/show.jsp";
+//	}
+	
+	@RequestMapping("/books/{id}")
+	public String show(@PathVariable("id") Long id, Model model) {
+	    Book book = bookService.findBook(id);
+	    model.addAttribute("book", book);
+	    return "/books/show.jsp";
+	}
+	
+	//CREATE BOOK
 	@RequestMapping("/books/new")
 	public String newBook(@ModelAttribute("book") Book book) {
 		return "/books/new.jsp";
@@ -47,4 +66,6 @@ public class BooksController {
 		}
 		
 	}
+	
+	
 }
